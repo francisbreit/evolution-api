466745148aae3d3341c989d46dbbcd8214a1f677
Francis Breit <francisbreit@gmail.com>
2024-06-11 16:19:09 -0300

    Update chatwoot-import-helper.ts

--- src/api/integrations/chatwoot/utils/chatwoot-import-helper.ts
+++ src/api/integrations/chatwoot/utils/chatwoot-import-helper.ts
@@ -104,19 +104,30 @@ class ChatwootImport {
   }
 
   public async insertTag(instanceName: string, totalContacts: number) {
-    const pgClient = postgresClient.getChatwootConnection();
-    const sqlInsertTag = `
-      INSERT INTO tags (name, taggings_count)
-      VALUES ($1, $2)
-      RETURNING id
-    `;
-    try {
+  const pgClient = postgresClient.getChatwootConnection();
+  const sqlCheckTag = `
+    SELECT id FROM tags WHERE name = $1
+  `;
+  const sqlInsertTag = `
+    INSERT INTO tags (name, taggings_count)
+    VALUES ($1, $2)
+    RETURNING id
+  `;
+
+  try {
+    const checkResult = await pgClient.query(sqlCheckTag, [instanceName]);
+    if (checkResult.rowCount === 0) {
       const result = await pgClient.query(sqlInsertTag, [instanceName, totalContacts]);
       return result.rows[0].id;
-    } catch (error) {
-      this.logger.error(`Error on insert tag: ${error.toString()}`);
+    } else {
+      this.logger.info(`Tag with name ${instanceName} already exists`);
+      return checkResult.rows[0].id;
     }
+  } catch (error) {
+    this.logger.error(`Error on insert tag: ${error.toString()}`);
   }
+}
+
 
   public async insertTaggings(tagId: number, contacts: ContactRaw[], createdAt: Date) {
     const pgClient = postgresClient.getChatwootConnection();
