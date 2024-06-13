25d6f10a68e5d2edbd13f8f00f6c0399c3e4c0fb
Francis Breit <francisbreit@gmail.com>
2024-06-13 07:49:05 -0300

    Update chatwoot-import-helper.ts

--- src/api/integrations/chatwoot/utils/chatwoot-import-helper.ts
+++ src/api/integrations/chatwoot/utils/chatwoot-import-helper.ts
@@ -115,9 +115,8 @@ public async insertTag(instanceName: string, totalContacts: number) {
     `;
     const sqlUpdateTag = `
         UPDATE tags
-        SET taggings_count = taggings_count + $2
+        SET taggings_count = taggings_count + 1
         WHERE name = $1
-
     `;
 
     try {
